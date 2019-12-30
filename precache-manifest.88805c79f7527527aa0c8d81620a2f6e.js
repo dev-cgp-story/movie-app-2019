032f5e5243339acdbc2e2c108a6436b2174ca33d
Kang MyeongShin <kangugi205@gmail.com>
2019-12-30 18:32:54 +0900

    Updates

---
+++ precache-manifest.88805c79f7527527aa0c8d81620a2f6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3bd5bb3829101fadef665d53a536fcd",
+    "url": "/movie-app-2019/index.html"
+  },
+  {
+    "revision": "ec304dfba479305781f0",
+    "url": "/movie-app-2019/static/css/main.de3553e0.chunk.css"
+  },
+  {
+    "revision": "b0e54b4fc26c42a447e7",
+    "url": "/movie-app-2019/static/js/2.d198053d.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/movie-app-2019/static/js/2.d198053d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ec304dfba479305781f0",
+    "url": "/movie-app-2019/static/js/main.c9eb7393.chunk.js"
+  },
+  {
+    "revision": "d2c0d5821df0102c306c",
+    "url": "/movie-app-2019/static/js/runtime-main.518e253b.js"
+  }
+]);
